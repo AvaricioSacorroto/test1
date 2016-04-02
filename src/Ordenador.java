a7de8e94399b254ca53f49521f50eb2bd3d967ef
Lina <Lina@192.168.0.13>
2016-04-02 12:27:44 +0200

    primera copia en linaprofe2

---
+++ src/Ordenador.java
@@ -0,0 +1,61 @@
+
+
+/**Ampliar:    Crear atributo static
+ *             método static mostrarGarantia()
+ * 
+ * @author ape
+ */
+public class Ordenador {
+    //atributos
+  private  String marca;
+   private String procesador;
+  private  String pantalla;
+  
+ private   boolean ordenadorEncendido;
+    
+private      static final int garantia=2;
+     //Constructores  
+    public Ordenador(){        
+    }
+   
+    public Ordenador(String marca, String procesador, String apagado){
+        System.out.println("\nESTADO DEL ORDENADOR:" +"\nMarca  "+ marca +"\nProcesador  "+procesador+"\n"+apagado);
+    }   
+      
+      
+    //metodos
+    void encenderOrdenador(){
+        if(ordenadorEncendido == true){
+            System.out.println("El ordenador ya está encendido");
+        }else{  //si no está encendido, encenderlo
+            ordenadorEncendido = true; 
+            System.out.println("\nEl ordenador se ha encendido");
+        }
+    }
+    void mostrarEstado(){
+        System.out.println("\nESTADO DEL ORDENADOR:" +"\nMarca  "+ marca +"\nProcesador  "+procesador+"\nPantalla  "+pantalla+"\n");
+        if(ordenadorEncendido == true){
+            System.out.println("\nEl ordenador está encendido.");
+        }else{  //si no está encendido...
+            System.out.println("El ordenador está apagado");
+        }
+    }
+    void apagarOrdenador(){
+        if(ordenadorEncendido == true){
+            ordenadorEncendido = false;
+            System.out.println("El ordenador se ha apagado.\n");
+        }
+    }
+   
+    void asignarPantalla(String pantalla){
+        System.out.println(pantalla);
+        
+    }
+    static public void mostrarGarantia(){
+         
+       System.out.println("Todos los ordenadores tiene de  garantía: "+ garantia);
+
+}
+
+    
+}
---
+++ test/OrdenadorTest.java
@@ -0,0 +1,97 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+/**
+ *
+ * @author ape
+ */
+public class OrdenadorTest {
+    
+    public OrdenadorTest() {
+    }
+
+    @BeforeClass
+    public static void setUpClass() throws Exception {
+    }
+
+    @AfterClass
+    public static void tearDownClass() throws Exception {
+    }
+    
+    @Before
+    public void setUp() {
+    }
+    
+    @After
+    public void tearDown() {
+    }
+
+    /**
+     * Test of encenderOrdenador method, of class Ordenador.
+     */
+    @Test
+    public void testEncenderOrdenador() {
+        System.out.println("encenderOrdenador");
+        Ordenador instance = new Ordenador();
+        instance.encenderOrdenador();
+        // TODO review the generated test code and remove the default call to fail.
+        fail("The test case is a prototype.");
+    }
+
+    /**
+     * Test of mostrarEstado method, of class Ordenador.
+     */
+    @Test
+    public void testMostrarEstado() {
+        System.out.println("mostrarEstado");
+        Ordenador instance = new Ordenador();
+        instance.mostrarEstado();
+        // TODO review the generated test code and remove the default call to fail.
+        fail("The test case is a prototype.");
+    }
+
+    /**
+     * Test of apagarOrdenador method, of class Ordenador.
+     */
+    @Test
+    public void testApagarOrdenador() {
+        System.out.println("apagarOrdenador");
+        Ordenador instance = new Ordenador();
+        instance.apagarOrdenador();
+        // TODO review the generated test code and remove the default call to fail.
+        fail("The test case is a prototype.");
+    }
+
+    /**
+     * Test of asignarPantalla method, of class Ordenador.
+     */
+    @Test
+    public void testAsignarPantalla() {
+        System.out.println("asignarPantalla");
+        String pantalla = "";
+        Ordenador instance = new Ordenador();
+        instance.asignarPantalla(pantalla);
+        // TODO review the generated test code and remove the default call to fail.
+        fail("The test case is a prototype.");
+    }
+
+    /**
+     * Test of mostrarGarantia method, of class Ordenador.
+     */
+    @Test
+    public void testMostrarGarantia() {
+        System.out.println("mostrarGarantia");
+        Ordenador.mostrarGarantia();
+        // TODO review the generated test code and remove the default call to fail.
+        fail("The test case is a prototype.");
+    }
+}
